e2ec71d55c48a3dc96405c5d7a9c322886650029
vnvZio7 <vungocvan.zio7.03@gmail.com>
2026-03-10 08:35:48 +0700

    Add file start

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello DevOps CI/CD";
+    }
+}
