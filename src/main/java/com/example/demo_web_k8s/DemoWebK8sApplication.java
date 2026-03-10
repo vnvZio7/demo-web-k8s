d87c9f1a9596ba172d9100738e29304e6fde9e99
vnvZio7 <vungocvan.zio7.03@gmail.com>
2026-03-10 08:25:47 +0700

    first commit

---
+++ src/main/java/com/example/demo_web_k8s/DemoWebK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_web_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_web_k8s/DemoWebK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_web_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
